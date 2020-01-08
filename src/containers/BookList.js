7ac5ab248bce62b6649220de570174e5ab44d1c1
bright okike <brightokike@gmail.com>
2020-01-08 13:26:32 +0300

    :art: added table to the BookList component
    added a JSX code
    added the Book ID, Title and Category
    linked the booklist component to the redux store
    added a html table

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const BookList = () => <div>BookList</div>;
+const BookList = () => (
 
-export default BookList;
+  <div>
+    <table>
+      <tr>
+        <th> Book ID </th>
+        <th> Title </th>
+        <th> Category </th>
+      </tr>
+      <tr>
+        <td />
+        <td />
+        <td />
+      </tr>
+    </table>
+  </div>
+);
+
+const mapStateToProps = (state) => ({ books: state.books });
+
+export default connect(mapStateToProps)(BookList);
