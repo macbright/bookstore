ab993dc72a3b17a6869c4a7097fb7f0ab50f4f67
bright okike <brightokike@gmail.com>
2020-01-08 14:12:13 +0300

    :art: added a html form to the bookstore component
    added html form
    added book title
    added book a select box book categories with a predefined constants
    added a submit button to the form

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,5 +1,33 @@
+/* eslint-disable no-tabs */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
-const BookForm = () => <div>BookForm</div>;
+const BookForm = () => (
+  <div>
+    <form>
+      <div>
+        <label>
+					Book Title:
+          <input type="text" placeHolder="enter book title" name="title" />
+        </label>
+      </div>
+      <div>
+        <select>
+          <option value="Action"> Action </option>
+          <option value="Biography"> Biography </option>
+          <option value="History"> History</option>
+          <option value="Horro"> Horror</option>
+          <option value="Kids"> Kids </option>
+          <option value="Learning"> Learning </option>
+          <option value="Sci-Fi"> Sci-Fi </option>
+        </select>
+      </div>
+      <div>
+        <input type="submit" value="submit" />
+      </div>
+    </form>
+  </div>
+);
+
 
 export default BookForm;
